b8267b2db91f9dc2821622b2326f906e2370ef92
Dimitar Jilanov <dimitar.jilanov@mobilewavesolutions.com>
2015-09-10 19:09:01 +0300

    V1 Integrate the info and clean the templates that we did'nt need

--- Game/client/router.js
+++ Game/client/router.js
@@ -1,21 +1,15 @@
 var app = require('ampersand-app');
 var Router = require('ampersand-router');
 var HomePage = require('./pages/home');
-var CollectionDemo = require('./pages/collection-demo');
+var game = require('./pages/collection-demo');
 var InfoPage = require('./pages/info');
-var PersonAddPage = require('./pages/person-add');
-var PersonEditPage = require('./pages/person-edit');
-var PersonShowPage = require('./pages/person-show');
 
 
 module.exports = Router.extend({
     routes: {
         '': 'home',
-        'collections': 'collectionDemo',
+        'game': 'game',
         'info': 'info',
-        'person/add': 'personAdd',
-        'person/:id': 'personView',
-        'person/:id/edit': 'personEdit',
         '(*path)': 'catchAll'
     },
 
@@ -26,32 +20,16 @@ module.exports = Router.extend({
         }));
     },
 
-    collectionDemo: function () {
-        app.trigger('page', new CollectionDemo({
-            model: app.me,
-            collection: app.people
-        }));
+    game: function () {
+        // app.trigger('page', new CollectionDemo({
+        //     model: app.me,
+        //     collection: app.people
+        // }));
     },
 
     info: function () {
         app.trigger('page', new InfoPage({
             model: app.me
-        }));
-    },
-
-    personAdd: function () {
-        app.trigger('page', new PersonAddPage());
-    },
-
-    personEdit: function (id) {
-        app.trigger('page', new PersonEditPage({
-            id: id
-        }));
-    },
-
-    personView: function (id) {
-        app.trigger('page', new PersonShowPage({
-            id: id
         }));
     },
 
--- Game/client/templates.js
+++ Game/client/templates.js
@@ -18,17 +18,12 @@
 
     // body.jade compiled template
     templatizer["body"] = function tmpl_body() {
-        return '<body><nav class="navbar navbar-default"><div class="container-fluid"><div class="navbar-header"><a href="/" class="navbar-brand">TicTacToe</a></div><ul class="nav navbar-nav"><li><a href="/">home</a></li><li><a href="/collections">collection demo</a></li><li><a href="/info">more info</a></li></ul></div></nav><div class="container"><main data-hook="page-container"></main><footer class="footer-main"><nav class="nav-footer cf"><div><a href="http://ampersandjs.com/learn" class="nav-item external">Learn</a><a href="http://ampersandjs.com/docs" class="nav-item external">Docs</a><a href="http://tools.ampersandjs.com" class="nav-item external">Modules</a></div><div><a href="https://gitter.im/AmpersandJS/AmpersandJS" class="nav-item external">Chatroom</a><a href="https://trello.com/b/UxylNzHr/ampersand-js-roadmap" class="nav-item external">Roadmap</a><a href="http://ampersandjs.com/contribute" class="nav-item external">Contribute</a></div><div><a href="http://ampersandjs.com/security" class="nav-item external">Security</a><a href="https://github.com/ampersandjs" class="nav-item external">Github</a><a href="https://twitter.com/ampersandjs" class="nav-item external">Twitter</a></div></nav><p>Sponsored by <a href="https://andyet.com">&amp;yet </a><br/>with the help of our <a href="http://ampersandjs.com/contribute">contributors</a></p><a href="http://ampersandjs.com" class="logo logo-ampersand-pink">&amp;</a></footer></div></body>';
+        return '<body><nav class="navbar navbar-default"><div class="container-fluid"><div class="navbar-header"><a href="/" class="navbar-brand">TicTacToe</a></div><ul class="nav navbar-nav"><li><a href="/">Home</a></li><li><a href="/game">Game</a></li><li><a href="/info">Info</a></li></ul></div></nav><div class="container"><main data-hook="page-container"></main><footer class="footer-main"><div>Developed by Dimitar Jilanov</div></footer></div></body>';
     };
 
     // head.jade compiled template
     templatizer["head"] = function tmpl_head() {
         return '<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0"/><meta name="apple-mobile-web-app-capable" content="yes"/>';
-    };
-
-    // includes\formInput.jade compiled template
-    templatizer["includes"]["formInput"] = function tmpl_includes_formInput() {
-        return '<div class="form-group"><label data-hook="label"></label><div data-hook="message-container"><div data-hook="message-text" class="alert alert-danger"></div></div><input class="form-control"/></div>';
     };
 
     // includes\person.jade compiled template
@@ -43,27 +38,12 @@
 
     // pages\home.jade compiled template
     templatizer["pages"]["home"] = function tmpl_pages_home() {
-        return '<section class="page home"><h2>Welcome to a skeleton for TicTacToe</h2><p>If you "view source" you\'ll see it\'s 100% client rendered.</p><p>Click around the site using the nav bar at the top. </p><p>Things to note:<ul><li>The url changes, no requests are made to the server.</li><li>Refreshing the page will always get you back to the same page</li><li>Page changes are nearly instantaneous</li><li>In development mode, you don\'t need to restart the server to see changes, just edit and refresh.</li><li>In production mode, it will serve minfied, uniquely named files with super agressive cache headers. To test:<ul> <li>in dev_config.json set <code>isDev</code> to <code>false</code>.</li><li>restart the server.</li><li>view source and you\'ll see minified css and js files with unique names.</li><li>open the "network" tab in chrome dev tools (or something similar). You\'ll also want to make sure you haven\'t disabled your cache.</li><li>without hitting "refresh" load the app again (selecting current URL in url bar and hitting "enter" works great).</li><li>you should now see that the JS and CSS files were both served from cache without making any request to the server at all.</li></ul></li></ul></p></section>';
+        return '<section class="page home"><h2>Welcome to the classic Tic Tac Toe game</h2><p>If you want to start a game click<a href="/game"> Game</a><span> from the navigation</span></p><p>If you want to check out the hints and tricks click<a href="/game"> Info</a><span> from the navigation</span></p><p>If you want to view the source, check it out<a href="https://github.com/DJilanov/TicTacToe"> here</a><span>.</span></p></section>';
     };
 
     // pages\info.jade compiled template
     templatizer["pages"]["info"] = function tmpl_pages_info() {
-        return '<section class="page pageTwo"><h2>Simple Page Example</h2><p>This page was rendered by a simple page view file at client/pages/info.js.</p></section>';
-    };
-
-    // pages\personAdd.jade compiled template
-    templatizer["pages"]["personAdd"] = function tmpl_pages_personAdd() {
-        return '<section class="page add-person"><h2>Add Person</h2><p>This form and all behavior is defined by the form view in <code>client/forms/person.js</code>.</p><p>The same form-view is used for both editing and creating new users.</p><form data-hook="person-form"><fieldset data-hook="field-container"></fieldset><div class="buttons"><button data-hook="reset" type="submit" class="btn">Submit</button></div></form></section>';
-    };
-
-    // pages\personEdit.jade compiled template
-    templatizer["pages"]["personEdit"] = function tmpl_pages_personEdit() {
-        return '<section class="page edit-person"><h2>Edit Person</h2><p>This form and all behavior is defined by the form view in <code>client/forms/person.js</code>.</p><p>The same form-view is used for both editing and creating new users.</p><form data-hook="person-form"><fieldset data-hook="field-container"></fieldset><div class="buttons"><button data-hook="reset" type="submit" class="btn">Submit</button></div></form></section>';
-    };
-
-    // pages\personView.jade compiled template
-    templatizer["pages"]["personView"] = function tmpl_pages_personView() {
-        return '<section class="page view-person"><h2 data-hook="name"></h2><img data-hook="avatar" width="80" height="80"/><div class="buttons"><a data-hook="edit" class="btn">Edit</a><button data-hook="delete" class="btn">Delete</button></div></section>';
+        return '<section class="page pageTwo"><h2>Info and tricks</h2><p>Tic-tac-toe (also known as Noughts and crosses or Xs and Os) is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.</p><p>The rules to win are simple:<ul><li> If the player has two in a row, they can place a third to get three in a row.</li><li> If the opponent has two in a row, the player must play the third themselves to block the opponent.</li><li> Create an opportunity where the player has two threats to win (two non-blocked lines of 2).</li><li> If there is a configuration where the opponent can fork, the player should block that fork.</li><li> A player marks the center. (If it is the first move of the game, playing on a corner gives "O" more opportunities to make a mistake and may therefore be the better choice; however, it makes no difference between perfect players.)</li><li> If the opponent is in the corner, the player plays the opposite corner.</li><li> The player plays in a corner square.</li><li> The player plays in a middle square on any of the 4 sides.</li></ul></p></section>';
     };
 
     return templatizer;
